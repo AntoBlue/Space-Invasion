a212f87b22697e8458fab2d1d9ca24dacf1db393
AntoBlue <antonio_r@hotmail.it>
2025-05-05 00:38:34 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerDestroyOnHit.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class PlayerDestroyOnHit : MonoBehaviour
+{
+	GameManager manager;
+	
+	public static Action OnPlayerDestroyed;
+
+	public GameManager Manager { get => manager; set => manager = value; }
+	//public GameManager Manager => manager;
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (!enabled) return;
+
+		if (collision.gameObject.CompareTag("EnemyBullet")) {
+
+			Debug.Log("Hit by Enemy bullet", this);
+
+			//GetComponentInParent<PlayerManager>().enabled = false;
+			//GetComponentInParent<MeshRenderer>().enabled = false;
+
+			OnPlayerDestroyed?.Invoke();
+			
+			Destroy(gameObject,2);
+
+			bool first = true;
+
+			foreach (Transform t in transform) {
+
+				Debug.Log(t.name);
+
+				if (first) {
+					first = false;
+					continue;
+				}				
+				t.gameObject.SetActive(true);
+
+				Rigidbody rb = t.gameObject.AddComponent<Rigidbody>();
+				rb.AddExplosionForce(20, t.transform.position, 100,0,ForceMode.Impulse);
+			}
+
+			Manager.GameOver();
+
+			enabled = false;
+		}
+	}
+}
